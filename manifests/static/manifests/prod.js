235376f38112fd02c70397ffe91549b9d28eb5ef
Chip Goines <chip_goines@harvard.edu>
2015-11-30 18:20:27 -0500

    readded contextmenu code

--- manifests/static/manifests/prod.js
+++ manifests/static/manifests/prod.js
@@ -541,8 +541,8 @@ $(function() {
     });
   };
 
-/*
-  $("canvas").contextmenu({
+
+  $('canvas').contextmenu({
     //delegate: ".openseadragon-canvas",
     menu: [ {title: "Save image", cmd: "save", uiIcon: "ui-icon-disk"} ],
     select: function(event, ui) {
@@ -552,7 +552,7 @@ $(function() {
         }
     }
   });
-*/
+
 
   $(document).on('click', "a.cite, a.view-in-pds, a.search, a.print, a.viewtext, a.links", present_choices);
 
