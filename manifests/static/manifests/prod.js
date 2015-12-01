b7672bbb1e595219e7fdcadef02873ece26aeb3c
Chip Goines <chip_goines@harvard.edu>
2015-11-30 20:34:22 -0500

    switched contextmenu to image-view class for testing

--- manifests/static/manifests/prod.js
+++ manifests/static/manifests/prod.js
@@ -542,7 +542,7 @@ $(function() {
   };
 
 
-  $('.mirador-osd' ).contextmenu({
+  $('.image-view' ).contextmenu({
     //delegate: ".openseadragon-canvas",
     menu: [ {title: "Save image", cmd: "save", uiIcon: "ui-icon-disk"} ],
     select: function(event, ui) {
