e580e346eb5c5e9810d832b5acd1c305325acfab
Chip Goines <chip_goines@harvard.edu>
2016-01-27 23:47:00 -0500

    test data for getcaption call

--- manifests/static/manifests/prod.js
+++ manifests/static/manifests/prod.js
@@ -565,7 +565,7 @@ $(function() {
        */
       var label = this.children[0].children[4].children[0].children[3].textContent;
       var drs_id = "400627084"; //TEST
-      $.getJSON( '/proxy/getcaption/' + drs_id + '?callback=?' )
+      getJSON( '/proxy/getcaption/' + drs_id + '?callback=?' )
         .done(function (data) {
           console.log("hello it me");
           /*if (data.caption) {
