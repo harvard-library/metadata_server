11eefcdf745fb7ec2bb40305c7cd299027a1dbae
Chip Goines <chip_goines@harvard.edu>
2016-07-11 16:58:05 -0400

    added timing code for debugging

--- manifests/webclient.py
+++ manifests/webclient.py
@@ -7,10 +7,10 @@ logger = getLogger(__name__)
 from timeit import default_timer as timer
 
 def get(url, cookie_value=None):
-    start = timer()
     opener = urllib2.build_opener()
     if cookie_value:
         opener.addheaders.append(('Cookie', 'hulaccess='+cookie_value))
+    start = timer()
     response = opener.open(url)
     end = timer()
     elapsed = end - start
