da9a2c81ecc666a42a0599af79423f422bd85654
Chip Goines <chip_goines@harvard.edu>
2016-07-29 17:14:41 -0400

    removed save as function

--- manifests/static/manifests/prod.js
+++ manifests/static/manifests/prod.js
@@ -554,7 +554,7 @@ $(function() {
     }, 'image/jpg');
   };
 
-
+/*
   $('.layout-slot').contextmenu({
     //delegate: ".openseadragon-canvas",
     menu: [ {title: "Save image", cmd: "save", uiIcon: "ui-icon-disk"} ],
@@ -594,7 +594,7 @@ $(function() {
        }
 
        if (drs_id == null) return;
-     /* canvas copy no longer used bc of cors/tainted canvas side effects
+     // canvas copy no longer used bc of cors/tainted canvas side effects
             var targetCanvas = this.children[0].children[4].children[1].children[2].children[1].children[7].children[0].children[0].children[1];
             targetCanvas.crossOriginPolicy = 'Anonymous';
             $.getJSON( '/proxy/getcaption/' + drs_id + '?callback=?' )
@@ -606,12 +606,13 @@ $(function() {
 	           copyCanvas(targetCanvas, label);
 	        }
              });
-      */
+      //
        var caption_url = l.PDS_VIEW_URL.replace("view","showcaption") + drs_id + '?n=' + n;
        window.open(caption_url,'');
       
     }
   });
+*/
 
   $(document).on('click', "a.cite, a.view-in-pds, a.search, a.print, a.viewtext, a.links", present_choices);
 
