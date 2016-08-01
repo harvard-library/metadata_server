af5945348a1cb56f7d4e8b6ca5ac914e59b24330
Chip Goines <chip_goines@harvard.edu>
2016-08-01 17:00:07 -0400

    check object and file max access flags if needed

--- manifests/ams.py
+++ manifests/ams.py
@@ -5,13 +5,17 @@ import re
 from os import environ
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 
-oliviaServletBase = environ.get("OLIVIA_SERVLET_BASE", "http://olivia.lib.harvard.edu:9016/olivia/servlet/OliviaServlet?storedProcedure=getRestrictFlag&callingApplication=call1&oliviaUserName=iiif&oracleID=")
+oliviaServletBase = environ.get("OLIVIA_SERVLET_BASE", "http://olivia.lib.harvard.edu:9016/olivia/servlet/OliviaServlet?storedProcedure=getRestrictFlagForObject&callingApplication=call1&oliviaUserName=iiif&oracleID=")
+oliviaServletBase2 = environ.get("OLIVIA_SERVLET_BASE2","http://olivia.lib.harvard.edu:9016/olivia/servlet/OliviaServlet?storedProcedure=getRestrictFlag&callingApplication=call1&oliviaUserName=iiif&oracleID=");
 amsRedirectBase = environ.get("AMS_REDIRECT_BASE","")
 
 
 def getAccessFlag(drsId):
     oliviaServletURL = oliviaServletBase + drsId	
+    oliviaServletURL2 = oliviaServletBase2 + drsId
     req = requests.get(oliviaServletURL)
+    if req.status_code != 200: #obj call failed, try file access flag
+        req = requests.get(oliviaServletURL2)
     regex = re.compile('Restrict Flag: ([A-Z])')
     match = regex.search(req.text)
     flag = None
