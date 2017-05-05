77cc92ed1ad10da0828c23f395cd9a969329b4c3
Chip Goines <chip_goines@harvard.edu>
2017-05-05 05:14:59 -0400

    fix order of precedence

--- inbox/urls.py
+++ inbox/urls.py
@@ -6,7 +6,7 @@ from inbox import views
 urlpatterns = patterns('', 
 		url(r'^$', views.index, name='index'),
 		url(r'^delete/(?P<notification_id>[a-z]+:[A-Za-z\d]+)$', views.delete, name='delete'),
-                url(r'^empty$', views.empty, name='empty') ),
+		url(r'^empty$', views.empty, name='empty') ),
 		url(r'^(?P<notification_id>[A-Za-z\d]+)$', views.get_notification)
 	      )
 
