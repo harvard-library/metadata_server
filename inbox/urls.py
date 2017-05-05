74abd55410c9f11eadb4c4ad21304f591d9d8230
Chip Goines <chip_goines@harvard.edu>
2017-05-05 03:46:30 -0400

    tried to add note id

--- inbox/urls.py
+++ inbox/urls.py
@@ -3,4 +3,8 @@ from django.conf import settings
 
 from inbox import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index') )
+urlpatterns = patterns('', 
+		url(r'^$', views.index, name='index'),
+		url(r'^inbox/(?P<notification_id>[A-Za-z\d]+)$', views.get_notification, name='get_notification')
+	      )
+
