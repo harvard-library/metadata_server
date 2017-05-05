ec268ef47bb36549cfded332935837ac829b59ce
Chip Goines <chip_goines@harvard.edu>
2017-05-05 04:15:14 -0400

    tried to add note id

--- inbox/urls.py
+++ inbox/urls.py
@@ -5,6 +5,6 @@ from inbox import views
 
 urlpatterns = patterns('', 
 		url(r'^$', views.index, name='index'),
-		url(r'^(?P<notification_id>[A-Za-z\d]+)$', views.get_notification, name="get_notification")
+		url(r'^(?P<notification_id>[A-Za-z\d]+)$', views.get_notification)
 	      )
 
