f96aa6b835fdeb7b3ca9b8c479c511dab5a607f7
Chip Goines <chip_goines@harvard.edu>
2017-05-05 05:37:42 -0400

    fixed delete path

--- inbox/urls.py
+++ inbox/urls.py
@@ -5,6 +5,6 @@ from inbox import views
 
 urlpatterns = patterns('', 
   url(r'^$', views.index, name='index'), 
-  url(r'^delete/(?P<notification_id>[a-z]+:[A-Za-z\d]+)$', views.delete, name='delete'), 
+  url(r'^delete/(?P<notification_id>[A-Za-z\d]+)$', views.delete, name='delete'), 
   url(r'^empty$', views.empty, name='empty'), 
   url(r'^(?P<notification_id>[A-Za-z\d]+)$', views.get_notification) )
