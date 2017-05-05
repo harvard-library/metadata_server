e4a862b9638bbc072a02e6ea0a1a02db3a253843
Chip Goines <chip_goines@harvard.edu>
2017-05-05 00:44:16 -0400

    adapted for inbox

--- inbox/models.py
+++ inbox/models.py
@@ -5,43 +5,44 @@ from elasticsearch import Elasticsearch
 # Create your models here.
 
 ELASTICSEARCH_URL = settings.ELASTICSEARCH_URL
-ELASTICSEARCH_INDEX = settings.ELASTICSEARCH_INDEX
+ELASTICSEARCH_INDEX = "notifications"
 ELASTICSEARCH_MAX_HIT_SIZE = settings.ELASTICSEARCH_MAX_HIT_SIZE
 
 # Connect to elasticsearch db
 def get_connection():
     return Elasticsearch(ELASTICSEARCH_URL)
 
-# Gets the content of a manifest, returns JSON
-def get_manifest(manifest_id, source):
+# Gets the content of a notification, returns JSON
+def get_notification(notification_id, source):
     es = get_connection()
-    return es.get(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id)["_source"]
+    return es.get(index=ELASTICSEARCH_INDEX, doc_type=source, id=notification_id)["_source"]
 
-# Inserts JSON document into elasticsearch with the given manifest_id
+# Inserts JSON document into elasticsearch with the given notification_id
 # Either adds new document or replaces existing document
-def add_or_update_manifest(manifest_id, document, source):
+def add_or_update_notification(notification_id, document, source):
     es = get_connection()
-    es.index(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id, body=document)
+    es.index(index=ELASTICSEARCH_INDEX, doc_type=source, id=notification_id, body=document)
 
-# Deletes manifest from elasticsearch (need to refresh index?)
-def delete_manifest(manifest_id, source):
+# Deletes notification from elasticsearch (need to refresh index?)
+def delete_notification(notification_id, source):
     es = get_connection()
-    es.delete(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id)
+    es.delete(index=ELASTICSEARCH_INDEX, doc_type=source, id=notification_id)
 
-# Checks if manifest exists in elasticsearch, returns boolean
-def manifest_exists(manifest_id, source):
+# Checks if notification exists in elasticsearch, returns boolean
+def notification_exists(notification_id, source):
     es = get_connection()
-    return es.exists(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id)
+    return es.exists(index=ELASTICSEARCH_INDEX, doc_type=source, id=notification_id)
 
-def get_all_manifest_ids_with_type(source):
+def get_all_notification_ids_for_target(target, source):
     es = get_connection()
-    results = es.search(index=ELASTICSEARCH_INDEX, doc_type=source, fields="[]", size=ELASTICSEARCH_MAX_HIT_SIZE)
+    results = es.search(index=ELASTICSEARCH_INDEX, doc_type=source, fields="[]", 
+      body={ 'query':{ 'match':{ 'target': target } } }, size=ELASTICSEARCH_MAX_HIT_SIZE)
     ids = []
     for r in results["hits"]["hits"]:
         ids.append(str(r["_id"]))
     return ids
 
-def get_all_manifest_ids():
+def get_all_notification_ids():
     es = get_connection()
     results = es.search(index=ELASTICSEARCH_INDEX, fields="[]", size=ELASTICSEARCH_MAX_HIT_SIZE)
     ids = []
@@ -49,6 +50,6 @@ def get_all_manifest_ids():
         ids.append(str(r["_id"]))
     return ids
 
-def get_manifest_title(manifest_id, source):
+def get_notification_title(notification_id, source):
     es = get_connection()
-    return es.get(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id)["_source"]["label"]
+    return es.get(index=ELASTICSEARCH_INDEX, doc_type=source, id=notification_id)["_source"]["label"]
