f6310687def423c2425fd4dae062c79008780b08
Chip Goines <chip_goines@harvard.edu>
2017-05-05 05:18:51 -0400

    fix order of precedence

--- inbox/urls.py
+++ inbox/urls.py
@@ -3,8 +3,4 @@ from django.conf import settings
 
 from inbox import views
 
-urlpatterns = patterns('', 
-		url(r'^$', views.index, name='index'),
-		url(r'^delete/(?P<notification_id>[a-z]+:[A-Za-z\d]+)$', views.delete, name='delete'),
-		url(r'^empty$', views.empty, name='empty') ),
-		url(r'^(?P<notification_id>[A-Za-z\d]+)$', views.get_notification) )
+urlpatterns = patterns('', url(r'^$', views.index, name='index'), url(r'^delete/(?P<notification_id>[a-z]+:[A-Za-z\d]+)$', views.delete, name='delete'), url(r'^empty$', views.empty, name='empty') ), url(r'^(?P<notification_id>[A-Za-z\d]+)$', views.get_notification) )
