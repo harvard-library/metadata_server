bf8663ca85e1480e5e0b7b6c11580e4aa315dfa8
Chip Goines <chip_goines@harvard.edu>
2017-05-23 20:56:21 -0400

    added received timestamp

--- inbox/views.py
+++ inbox/views.py
@@ -83,7 +83,7 @@ def do_post(request):
   except:
     return HttpResponse("Invalid target %s \n" % drs_id, status=500)
 
-  received = datetime.fromtimestamp(int(time())).strftime('%d-%m-%Y %I:%M:%S %Z')
+  received = datetime.fromtimestamp(int(time())).strftime('%m-%d-%Y %H:%M:%S %z')
   document['received'] = received
 
   #add to elasticsearch
