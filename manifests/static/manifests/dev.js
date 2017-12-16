55fb9a699d5a3177d733128fd40d31a4db1b28ad
Chip Goines <chip_goines@harvard.edu>
2017-12-15 20:14:06 -0500

    added showLabel: false to dialogBaseOpts

--- manifests/static/manifests/dev.js
+++ manifests/static/manifests/dev.js
@@ -8,6 +8,7 @@ $(function() {
     draggable: false,
     resizable: false,
     width: "50%",
+    showLabel: false,
     classes: "qtip-bootstrap",
     close: function (e) { $(this).remove()}
   };
