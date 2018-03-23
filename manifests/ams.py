1a8c9ed4f2b3ddda2707e9cd2bbc21aa6acc0ab7
Chip Goines <chip_goines@harvard.edu>
2018-03-22 22:40:23 -0400

    removed olivia dependency

--- manifests/ams.py
+++ manifests/ams.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python
 
 import urllib2, requests
-import re
+import re, json
 from os import environ
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+from django.conf import settings
 
-oliviaServletBase = environ.get("OLIVIA_SERVLET_BASE", "http://olivia.lib.harvard.edu:9016/olivia/servlet/OliviaServlet?storedProcedure=getRestrictFlagForObject&callingApplication=call1&oliviaUserName=iiif&oracleID=")
-oliviaServletBase2 = environ.get("OLIVIA_SERVLET_BASE2","http://olivia.lib.harvard.edu:9016/olivia/servlet/OliviaServlet?storedProcedure=getRestrictFlag&callingApplication=call1&oliviaUserName=iiif&oracleID=");
+#replace olivia w/ http://libsearch-elb.lib.harvard.edu:18280/solr/drs2-collection/select?q=object_id_num%3A434603714&fq=doc_type_string%3Aobject+AND+object_huldrsadmin_status_string%3Acurrent+AND+object_huldrsadmin_contentModelID_string%3ACMID-4.0&fl=object_id_num%2C+object_huldrsadmin_accessFlag_string&wt=json&indent=true
+
 amsRedirectBase = environ.get("AMS_REDIRECT_BASE","")
 
-
 def getAccessFlag(drsId):
-    oliviaServletURL = oliviaServletBase + drsId	
-    oliviaServletURL2 = oliviaServletBase2 + drsId
-    req = requests.get(oliviaServletURL)
-    if req.status_code != 200: #obj call failed, try file access flag
-        req = requests.get(oliviaServletURL2)
-    regex = re.compile('Restrict Flag: ([A-Z])')
-    regex2 = re.compile('Is Drs2: ([YN])')
-    match = regex.search(req.text)
-    match2 = regex2.search(req.text) 
-    flag = None
-    drs2 = None
-
-    if match2:
-	drs2 = match2.group(1)
-    if match:
-        flag = match.group(1)
-    if flag:
-        return [flag, drs2]
+    solrUrl = settings.SOLR_BASE + settings.SOLR_QUERY_PREFIX + document_id + settings.SOLR_AMS_QUERY 
+    req = requests.get(solrUrl)
+    if req.status_code == 200:
+	md_json = json.loads(req.read())
+	flag = md_json['response']['docs'][0]['object_huldrsadmin_accessFlag_string']
+	return flag
     else:
-        return ''
+	return ''
 
 def checkCookie(cookies, drsId):
     if 'hulaccess' in cookies:
@@ -42,9 +30,9 @@ def getAMSredirectUrl(cookies, drsId):
     flag = getAccessFlag(drsId)
     if flag == '':
 	return None
-    if flag[0] == 'R':
-        return ['R', flag[1], checkCookie(cookies, drsId)]
-    elif flag[0] == 'N':
+    if flag == 'R':
+        return ['R', checkCookie(cookies, drsId)]
+    elif flag == 'N':
 	return ['N', None]
     else:
-    	return ['OK', flag[1]]
+    	return ['OK', None]
