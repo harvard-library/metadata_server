1914bb471ed7e631b0bb729c2a02aeb126577b0e
Chip Goines <chip_goines@harvard.edu>
2018-04-10 19:45:35 -0400

    share and download plugin code included

--- manifests/static/manifests/prod.js
+++ manifests/static/manifests/prod.js
@@ -674,4 +674,214 @@ $(function() {
     History.replaceState({}, document.title, constructUrl(data.id));
   });
 
+/* Download plugin code */
+var DownloadButton = {
+  downloadBaseUrl: "https://download.digitale-sammlungen.de/BOOKS/download.pl?",
+  manifestUrlPattern: /^https?:\/\/api(?:-dev|-stg)?\.digitale-sammlungen\.de\/iiif\/presentation\/v2\/(bsb.*?)\/manifest/,
+  imageUrlTemplate: hMirador.Handlebars.compile("{{imageBaseUrl}}/full/{{size}}/0/default.jpg"),
+  buttonTemplate: hMirador.Handlebars.compile(['<span class="mirador-btn mirador-icon-download" role="button" title="Download">', '<i class="fa fa-download fa-lg fa-fw"></i>', '<i class="fa fa-caret-down"></i>', '<ul class="dropdown download-list">', '<li title="PDF-Download"><a href="{{downloadUrl}}" target="_blank">', '<i class="fa fa-file-pdf-o fa-lg fa-fw"></i>PDF-Download', "</a></li>", '<li title="IIIF-Manifest"><a href="{{manifestUrl}}" target="_blank">', '<i class="fa fa-file-text-o fa-lg fa-fw"></i>IIIF-Manifest', "</a></li>", "{{#each metadataUrls}}", '<li title="({{this.title}})">', '<a href="{{this.href}}" target="_blank">', '<i class="fa fa-file-text-o fa-lg fa-fw"></i>{{this.title}}', "</a></li>", "{{/each}}", "{{#each imageUrls}}", '<li class="{{#if (eq this "#")}}disabled {{/if}}image-link" title="JPG ({{this.title}})">', '<a href="{{this.href}}" target="_blank">', '<i class="fa fa-file-image-o fa-lg fa-fw"></i>JPG (<span class="dimensions">{{this.title}}</span>)', "</a></li>", "{{/each}}", "</ul>", "</span>"].join("")),
+  extendDownloadUrl: function(i, t, e) {
+    var a = this.downloadBaseUrl;
+    return -1 === i.indexOf("_") ? a += "id=" + i : a += "id=" + i.split("_")[0] + "&ersteseite=" + t + "&letzteseite=" + e, a
+  },
+  extractImageUrls: function(i) {
+    var t = i.imagesList[i.focusModules.ImageView.currentImgIndex],
+        e = hMirador.Iiif.getImageUrl(t),
+        a = t.height / t.width,
+        n = [];
+    return ["full", "250,"].forEach(function(l) {
+      n.push({
+        href: "ImageView" !== i.currentImageMode ? "#" : this.imageUrlTemplate({
+          imageBaseUrl: e,
+          size: l
+        }),
+        title: "full" === l ? t.width + "x" + t.height : parseInt(l) + "x" + Math.ceil(parseInt(l) * a)
+      })
+    }.bind(this)), n
+  },
+  extractMetadataUrls: function(i) {
+    if (!Array.isArray(i)) return [];
+    var t = {
+      "application/marcxml+xml": "MarcXML",
+      "application/rdf+xml": "RDF/XML"
+    };
+    return i.reduce(function(i, e) {
+      return e.format && /(application\/marcxml\+xml)|(application\/rdf\+xml)/.test(e.format) && i.push({
+        href: e["@id"],
+        title: t[e.format]
+      }), i
+    }, [])
+  },
+  init: function() {
+    hMirador.Handlebars.registerHelper("eq", function(i, t) {
+      return i === t
+    }), this.injectWindowEventHandler(), this.injectWorkspaceEventHandler()
+  },
+  injectButtonToMenu: function(i, t, e, a, n) {
+    $(i).prepend(this.buttonTemplate({
+      downloadUrl: t,
+      imageUrls: a,
+      manifestUrl: e,
+      metadataUrls: n
+    }))
+  },
+  injectWindowEventHandler: function() {
+    var i = this,
+        t = hMirador.Window.prototype.bindNavigation;
+    hMirador.Window.prototype.bindNavigation = function() {
+      t.apply(this), this.element.find(".window-manifest-navigation").on("mouseenter", ".mirador-icon-download", function() {
+        this.element.find(".download-list").stop().slideFadeToggle(300)
+      }.bind(this)).on("mouseleave", ".mirador-icon-download", function() {
+        this.element.find(".download-list").stop().slideFadeToggle(300)
+      }.bind(this))
+    };
+    var e = hMirador.Window.prototype.bindEvents;
+    hMirador.Window.prototype.bindEvents = function() {
+      e.apply(this), this.eventEmitter.subscribe("windowUpdated", function(t, e) {
+        if (this.id === e.id && e.viewType) if ("ImageView" === e.viewType) {
+          var a = i.extractImageUrls(this);
+          this.element.find(".image-link").removeClass("disabled").attr("title", function(i) {
+            return "JPG (" + a[i].title + ")"
+          }).find("a").attr("href", function(i) {
+            return a[i].href
+          }).find("span.dimensions").text(function(i) {
+            return a[i].title
+          })
+        } else this.element.find(".image-link").addClass("disabled")
+      }.bind(this))
+    }
+  },
+  injectWorkspaceEventHandler: function() {
+    var i = this,
+        t = hMirador.Workspace.prototype.bindEvents;
+    hMirador.Workspace.prototype.bindEvents = function() {
+      t.apply(this), this.eventEmitter.subscribe("windowAdded", function(t, e) {
+        var a = this.windows.filter(function(i) {
+          return i.id === e.id
+        })[0],
+            n = a.manifest.jsonLd["@id"],
+            l = n.match(i.manifestUrlPattern);
+        if (null !== l) {
+          var r = l[1],
+              o = a.manifest.jsonLd.sequences[0].canvases,
+              s = o[0].images[0].resource["@id"].split("_").pop().split("/").shift(),
+              d = o[o.length - 1].images[0].resource["@id"].split("_").pop().split("/").shift(),
+              f = i.extendDownloadUrl(r, s, d),
+              c = i.extractImageUrls(a),
+              m = i.extractMetadataUrls(a.manifest.jsonLd.seeAlso),
+              p = a.element.find(".window-manifest-navigation");
+          i.injectButtonToMenu(p, f, n, c, m)
+        }
+      }.bind(this))
+    }
+  }
+};
+$(document).ready(function() {
+  DownloadButton.init()
+});
+
+
+
+/* Share plugin code */
+var ShareButtons = {
+  /* all of the needed locales */
+  locales: {
+    'en': {
+      'share-buttons-info': 'By clicking on one of the share buttons, you will leave this website.',
+      'share-on-envelope': 'Share via mail',
+      'share-on-facebook': 'Share on Facebook',
+      'share-on-pinterest': 'Share on Pinterest',
+      'share-on-tumblr': 'Share on Tumblr',
+      'share-on-twitter': 'Share on Twitter',
+      'share-on-whatsapp': 'Share via Whatsapp'
+    }
+  },
+
+  /* options of the plugin */
+  showExternalLinkInfo: false,
+
+  /* the template for the buttons */
+  buttonsTemplate: hMirador.Handlebars.compile([
+    '{{#if showExternalLinkInfo}}',
+    '<div id="share-buttons-info" class="alert alert-info" role="alert">{{t "share-buttons-info"}}</div>',
+    '{{/if}}',
+    '<div id="share-buttons" class="pull-left">',
+    '{{#each shareButtons}}',
+    '<a type="button" class="share-button" id="share-on-{{this}}" title="{{t (concat this)}}" target="_blank" data-target="{{this}}">',
+    '<span class="fa-stack fa-lg">',
+    '<i class="fa fa-circle fa-stack-2x"></i>',
+    '<i class="fa fa-{{this}} fa-stack-1x" aria-hidden="true"></i>',
+    '</span>',
+    '</a>',
+    '{{/each}}',
+    '</div>'
+  ].join('')),
+
+  /* the templates for the different links */
+  linkTemplates: {
+    'envelope': hMirador.Handlebars.compile(
+      'mailto:?subject={{{label}}}{{#if attribution}} ({{attribution}}){{/if}}&body={{{label}}}{{#if attribution}} ({{{attribution}}}){{/if}}: {{link}}'
+    ),
+    'facebook': hMirador.Handlebars.compile(
+      'https://www.facebook.com/sharer/sharer.php?title={{{label}}} {{#if attribution}} ({{attribution}}){{/if}}&u={{link}}'
+    ),
+    'pinterest': hMirador.Handlebars.compile(
+      'http://pinterest.com/pin/create/bookmarklet/?url={{link}}&description={{{label}}}%20{{#if attribution}}%20({{attribution}}){{/if}}&media={{{thumbnailUrl}}}'
+    ),
+    'tumblr': hMirador.Handlebars.compile(
+      'http://www.tumblr.com/share/link?url={{link}}&name={{{label}}} {{#if attribution}} ({{attribution}}){{/if}}&tags=iiif'
+    ),
+    'twitter': hMirador.Handlebars.compile(
+      'https://twitter.com/intent/tweet?text={{{truncate label attribution}}}&url={{link}}&hashtags=iiif'
+    ),
+    'whatsapp': hMirador.Handlebars.compile(
+      'whatsapp://send?text={{{label}}} {{#if attribution}} ({{attribution}}){{/if}}: {{link}}'
+    )
+  },
+
+  /* initializes the plugin */
+  init: function(showExternalLinkInfo){
+    hMirador.Handlebars.registerHelper('concat', function(target){
+      return 'share-on-' + target;
+    });
+    hMirador.Handlebars.registerHelper('truncate', function(label, attribution){
+      var text = label + (attribution ? ' (' + attribution + ')' : '');
+      if(text.length > 60){
+        text = text.substring(0, 60) + '...';
+      }
+      return text;
+    });
+    this.showExternalLinkInfo = showExternalLinkInfo || false;
+  },
+
+  /* injects the buttons to the target selector element */
+  injectButtonsToDom: function(targetSelector, position){
+    var shareButtons = ['facebook', 'twitter', 'pinterest', 'tumblr', 'envelope'];
+    if('ontouchstart' in window || navigator.maxTouchPoints){
+      shareButtons.push('whatsapp');
+    }
+    if(position === undefined || ['beforebegin', 'afterbegin', 'beforeend', 'afterend'].indexOf(position) === -1){
+      position = 'afterbegin';
+    }
+    document.querySelector(targetSelector).insertAdjacentHTML(position, this.buttonsTemplate({
+      'shareButtons': shareButtons,
+      'showExternalLinkInfo': this.showExternalLinkInfo
+    }));
+  },
+
+  /* updates the button links with the given parameters */
+  updateButtonLinks: function(data){
+    var this_ = this;
+    $('#share-buttons > .share-button').attr('href', function(){
+      return this_.linkTemplates[$(this).data('target')]({
+        'label': data.label,
+        'attribution': data.attribution,
+        'link': data.link,
+        'thumbnailUrl': data.thumbnailUrl
+      });
+    });
+  }
+};
+
+
 });
