3f386df45be8564758eda768beb7c5b5fe838ccd
Chip Goines <chip_goines@harvard.edu>
2018-04-21 01:27:17 -0400

    remove target='_blank' from href

--- manifests/static/download/downloadMenu.js
+++ manifests/static/download/downloadMenu.js
@@ -15,7 +15,7 @@ var DownloadButton = {
     '</a></li>',
     '{{#each imageUrls}}',
     '<li class="{{#if (eq this "#")}}disabled {{/if}}image-link" title="JPG ({{this.title}})">',
-    '<a href="{{this.href}}" target="_blank">',
+    '<a href="{{this.href}}">',
     '<i class="fa fa-file-image-o fa-lg fa-fw"></i>JPG (<span class="dimensions">{{this.title}}</span>)',
     '</a></li>',
     '{{/each}}',
