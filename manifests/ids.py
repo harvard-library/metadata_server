394669cc3a6938e2ebfb5639890a56d1281570f7
Chip Goines <chip_goines@harvard.edu>
2018-08-09 18:33:18 -0400

    call ids for info.json dimensions if missing from solr feed

--- manifests/ids.py
+++ manifests/ids.py
@@ -90,7 +90,7 @@ def main(data, document_id, source, host, cookie=None):
 		except:
 		  pass
 
-		if ( ('file_mix_imageHeight_num' not in cvs.keys()) or ('file_mix_imageWidth_num' not in cvs.keys()) ):
+		if ( ('file_mix_imageHeight_num' not in cvs) or ('file_mix_imageWidth_num' not in cvs) ):
 		    try: #call ids for info.json dimensions if missing from solr feed
 		      infoReq = requests.get(imageUriBase + str(cvs['file_id_num']) + '/info.json')
 		      if (infoReq.status_code == 200):
