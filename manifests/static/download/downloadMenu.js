bbe0d61c75c8ca94b14108cab00755bad0825063
Chip Goines <chip_goines@harvard.edu>
2018-08-10 13:38:23 -0400

    bracket typo

--- manifests/static/download/downloadMenu.js
+++ manifests/static/download/downloadMenu.js
@@ -64,16 +64,16 @@ var DownloadButton = {
     //['full', '250,'].forEach(function(size){
     ['300','600','1200','2400'].forEach(function(size){
 	var sHeight = Math.ceil(parseInt(size) * ratio);
-	if ( (size < maxWidth) && (sHeight < maxHeight)) {
-         imageUrls.push({
-          'href': viewerWindow.currentImageMode !== 'ImageView' ? '#' : this.imageUrlTemplate({
+	if ((size < maxWidth) && (sHeight < maxHeight)) {
+          imageUrls.push({
+            'href': viewerWindow.currentImageMode !== 'ImageView' ? '#' : this.imageUrlTemplate({
             'imageBaseUrl': imageBaseUrl, 'size': size
-          }),
-          'title': size === 'full' ? currentImage.width + 'x' + currentImage.height : parseInt(size) + ' x ' + sHeight,
-          'sizeLabel': sizeLabels[parseInt(size)], 'id': "download_" + parseInt(size)
-          });
-         }.bind(this));
-	}
+            }),
+            'title': size === 'full' ? currentImage.width + 'x' + currentImage.height : parseInt(size) + ' x ' + sHeight,
+            'sizeLabel': sizeLabels[parseInt(size)], 'id': "download_" + parseInt(size)
+            });
+        }
+    }.bind(this));
 
     return imageUrls;
 
