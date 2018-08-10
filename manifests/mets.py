25722ebf129b62da389b71bebc2321bd3a105150
Chip Goines <chip_goines@harvard.edu>
2018-08-10 16:53:59 -0400

    iiif prez v2

--- manifests/mets.py
+++ manifests/mets.py
@@ -557,7 +557,7 @@ def main(data, document_id, source, host, cookie=None):
 	rangeInfo = [{"Table of Contents" : rangeList}]
 
 	mfjson = {
-		"@context":"http://iiif.io/api/presentation/2.1/context.json",
+		"@context":"http://iiif.io/api/presentation/2/context.json",
 		"@id": manifest_uri,
 		"@type":"sc:Manifest",
 		"label":manifestLabel,
