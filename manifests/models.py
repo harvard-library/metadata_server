5f959c0c3d61dbefdaa170c6f61c2592491d99be
Chip Goines <chip_goines@harvard.edu>
2019-09-13 13:36:26 -0400

    rewrite of db layer- use mongo instead of elasticsearch

--- manifests/models.py
+++ manifests/models.py
@@ -1,76 +1,96 @@
 from django.db import models
 from django.conf import settings
-from elasticsearch import Elasticsearch
 import certifi
+import ssl
+import json
+from pymongo import MongoClient
 
-# Create your models here.
-
-ELASTICSEARCH_URL = settings.ELASTICSEARCH_URL
-ELASTICSEARCH_INDEX = settings.ELASTICSEARCH_INDEX
-ELASTICSEARCH_MAX_HIT_SIZE = settings.ELASTICSEARCH_MAX_HIT_SIZE
-ELASTICSEARCH_SSL = settings.ELASTICSEARCH_SSL
-ELASTICSEARCH_SSL_VERIFY = settings.ELASTICSEARCH_SSL_VERIFY
+MONGO_URL = settings.MONGO_URL
+MONGO_INDEX = settings.MONGO_INDEX
+MONGO_SSL_CERT = settings.MONGO_SSL_CERT
 
 #indexes per doctype. now have to use separate indexes for elasticsearch 6.x+
-INDEX_TYPE = {"drs": ELASTICSEARCH_INDEX, "via": "via", "hollis": "hollis", "huam" : "huam", "ext": "ext", "ids": "ids" }
+INDEX_TYPE = {"drs": MONGO_INDEX, "via": "via", "hollis": "hollis", "huam" : "huam", "ext": "ext", "ids": "ids" }
 
-# Connect to elasticsearch db
+#Connect to mongo db
 def get_connection():
-    if (ELASTICSEARCH_SSL):
-      return Elasticsearch(ELASTICSEARCH_URL, use_ssl=ELASTICSEARCH_SSL, verify_certs=ELASTICSEARCH_SSL_VERIFY, ca_certs=certifi.where())
-    else:
-      return Elasticsearch(ELASTICSEARCH_URL)
+  client = MongoClient(MONGO_URL, ssl=True, ssl_cert_reqs=ssl.CERT_REQUIRED, ssl_ca_certs=MONGO_SSL_CERT)
+  return client
 
 # Gets the content of a manifest, returns JSON
 def get_manifest(manifest_id, source):
-    es = get_connection()
-    idx = get_index(source)
-    return es.get(index=idx, doc_type=source, id=manifest_id)["_source"]
+  mg = get_connection()
+  idx = get_index(source)
+  db = mg["manifests"]
+  col = db[idx]
+  query  = { "id": manifest_id }
+  doc = col.find(query)
+  manifest = doc[0]['manifest']
+  mg.close()
+  return manifest
 
 # Inserts JSON document into elasticsearch with the given manifest_id
 # Either adds new document or replaces existing document
 def add_or_update_manifest(manifest_id, document, source):
-    es = get_connection()
-    idx = get_index(source)
-    es.index(index=idx, doc_type=source, id=manifest_id, body=document)
+  mg = get_connection()
+  idx = get_index(source)
+  db = mg["manifests"]
+  col = db[idx]
+  record = { "id": manifest_id , "manifest": document }
+  col.insert_one(record)
+  mg.close()
 
-# Deletes manifest from elasticsearch (need to refresh index?)
+# Deletes manifest from mongo (need to refresh index?)
 def delete_manifest(manifest_id, source):
-    es = get_connection()
-    idx = get_index(source)
-    es.delete(index=idx, doc_type=source, id=manifest_id)
+  mg  = get_connection()
+  idx = get_index(source)
+  db = mg["manifests"]
+  query  = { "id": manifest_id }
+  col = db[idx]
+  col.delete_one(query)
+  mg.close()
 
 # Checks if manifest exists in elasticsearch, returns boolean
 def manifest_exists(manifest_id, source):
-    es = get_connection()
-    idx = get_index(source)
-    return es.exists(index=idx, doc_type=source, id=manifest_id)
+  mg = get_connection()
+  idx = get_index(source)
+  db = mg["manifests"]
+  col = db[idx]
+  query  = { "id": manifest_id }
+  doc = col.find_one(query)  
+  mg.close()
+  if (doc == None):
+    return False
+  else:
+   return True
 
 def get_all_manifest_ids_with_type(source):
-    es = get_connection()
-    idx = get_index(source)
-    results = es.search(index=idx, doc_type=source, fields="[]", size=ELASTICSEARCH_MAX_HIT_SIZE)
-    ids = []
-    for r in results["hits"]["hits"]:
-        ids.append(str(r["_id"]))
-    return ids
-
-def get_all_manifest_ids():
-    es = get_connection()
-    results = es.search(index=ELASTICSEARCH_INDEX, fields="[]", size=ELASTICSEARCH_MAX_HIT_SIZE)
-    ids = []
-    for r in results["hits"]["hits"]:
-        ids.append(str(r["_id"]))
-    return ids
+  mg = get_connection()
+  idx = get_index(source)
+  db = mg["manifests"]
+  col = db[idx]
+  ids = []
+  manifests = col.find()
+  for m in manifests:
+    ids.append(str(m['id'])) 
+  mg.close()
+  return ids
 
 def get_manifest_title(manifest_id, source):
-    es = get_connection()
-    idx = get_index(source)
-    return es.get(index=idx, doc_type=source, id=manifest_id)["_source"]["label"]
+  mg = get_connection()
+  idx = get_index(source)
+  db = mg["manifests"]
+  col = db[idx]
+  query  = { "id": manifest_id }
+  doc = col.find(query)
+  manifest = doc[0]['manifest']
+  mg.close()
+  mf = json.loads(manifest)
+  return mf["label"]
 
-#get appropriate index. now use separate indexes for elasticsearch 6.x+
+#get appropriate collection
 def get_index(source):
     idx = INDEX_TYPE[source]
     if (idx == None):
-        idx = ELASTICSEARCH_INDEX
+        idx = MONGO_INDEX
     return idx
