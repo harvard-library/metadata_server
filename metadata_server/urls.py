19ab2aaa3400e6b66a7b557fbbe9338e52e72c2b
Chip Goines <chip_goines@harvard.edu>
2024-01-21 00:35:58 -0500

    updated for python > 3.9

--- metadata_server/urls.py
+++ metadata_server/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 from manifests import views
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'metadata_server.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -15,4 +15,4 @@ urlpatterns = patterns('',
     # Same terrible hack from manifests/urls.py, empty view_type is NON OPTIONAL
     url(r'^(?P<view_type>)images/(?P<filename>.*)$', views.get_image),
     url(r'^(?P<view_type>)+.*skins.*$', views.clean_url),
-)
+]
