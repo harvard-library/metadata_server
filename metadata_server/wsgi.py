1ce8becd7671795c82d0f621629ea65cbb552455
Chip Goines <chip_goines@harvard.edu>
2024-01-21 13:56:46 -0500

    imports for python >= 3.10

--- metadata_server/wsgi.py
+++ metadata_server/wsgi.py
@@ -10,5 +10,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metadata_server.settings")
 
+import collections.abc
+# imports for python 3.10 and higher.
+collections.Iterator = collections.abc.Iterator
+collections.Iterable = collections.abc.Iterable
+collections.Mapping = collections.abc.Mapping
+collections.MutableSet = collections.abc.MutableSet
+collections.MutableMapping = collections.abc.MutableMapping
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
